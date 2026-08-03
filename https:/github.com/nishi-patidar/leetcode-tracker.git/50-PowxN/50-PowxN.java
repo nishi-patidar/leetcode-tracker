0e4b387a726b06764910524e1907c603e92563df
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:49:36 +0530

    Create 50-PowxN

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/50-PowxN/50-PowxN.java
@@ -0,0 +1,21 @@
+// Last updated: 8/3/2026, 12:49:38 PM
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+        
+        double result = 1.0;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            power /= 2;
+        }
+        
+        return result;
+    }
+}
