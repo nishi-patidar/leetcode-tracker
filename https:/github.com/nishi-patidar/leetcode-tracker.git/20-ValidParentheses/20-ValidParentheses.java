d982c09810fbd03c3cacf2e64c188c02c4e8d61e
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:51:19 +0530

    Create 20-ValidParentheses

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,22 @@
+// Last updated: 8/3/2026, 12:51:22 PM
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
