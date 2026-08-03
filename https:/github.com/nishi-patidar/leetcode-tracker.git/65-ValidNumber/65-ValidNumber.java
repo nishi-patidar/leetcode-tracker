c73ac4d0a20859bccd0b4dba5bd3e9a3e0076899
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:48:46 +0530

    Create 65-ValidNumber

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/65-ValidNumber/65-ValidNumber.java
@@ -0,0 +1,35 @@
+// Last updated: 8/3/2026, 12:48:49 PM
+class Solution {
+    public boolean isNumber(String s) {
+        boolean seenDigit = false;
+        boolean seenExponent = false;
+        boolean seenDot = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c >= '0' && c <= '9') {
+                seenDigit = true;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else if (c == 'e' || c == 'E') {
+                if (seenExponent || !seenDigit) {
+                    return false;
+                }
+                seenExponent = true;
+                seenDigit = false;
+            } else if (c == '.') {
+                if (seenDot || seenExponent) {
+                    return false;
+                }
+                seenDot = true;
+            } else {
+                return false;
+            }
+        }
+        
+        return seenDigit;
+    }
+}
