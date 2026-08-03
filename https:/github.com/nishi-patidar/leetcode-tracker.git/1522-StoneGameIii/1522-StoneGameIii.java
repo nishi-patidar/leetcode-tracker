684ebac8b098e89215cc79565242e2c1d467c90f
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:47:47 +0530

    Create 1522-StoneGameIii

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1522-StoneGameIii/1522-StoneGameIii.java
@@ -0,0 +1,24 @@
+// Last updated: 8/3/2026, 12:47:50 PM
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+            for (int j = 0; j < 3 && i + j < n; j++) {
+                sum += stoneValue[i + j];
+                dp[i] = Math.max(dp[i], sum - dp[i + j + 1]);
+            }
+        }
+        
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+}
