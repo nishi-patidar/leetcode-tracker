68d3d1f33be4d7a901408faba03682255580b478
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:48:33 +0530

    Create 69-Sqrtx

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,25 @@
+// Last updated: 8/3/2026, 12:48:36 PM
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        
+        int left = 1;
+        int right = x;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid < x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return right;
+    }
+}
