b0e799abb1b1d6679d54824729f37029723603c6
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:48:41 +0530

    Create 67-AddBinary

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/67-AddBinary/67-AddBinary.java
@@ -0,0 +1,25 @@
+// Last updated: 8/3/2026, 12:48:44 PM
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
