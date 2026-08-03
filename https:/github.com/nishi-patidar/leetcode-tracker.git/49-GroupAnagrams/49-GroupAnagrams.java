cb232b1da7fdffd9eda76c7eba3f8e508ad25ca4
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:49:39 +0530

    Create 49-GroupAnagrams

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,25 @@
+// Last updated: 8/3/2026, 12:49:42 PM
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) {
+                map.put(keyStr, new ArrayList<>());
+            }
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
