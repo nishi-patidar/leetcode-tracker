bde168ab7c63e35c0247f3b9ddc4fbe9aa86e4bd
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-03 12:47:43 +0530

    Create 1574-MaximumProductOfTwoElementsInAnArray

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,19 @@
+// Last updated: 8/3/2026, 12:47:45 PM
+class Solution {
+        public int maxProduct(int[] nums) {
+                int max1 = 0;
+                        int max2 = 0;
+                                
+                                        for (int n : nums) {
+                                                    if (n > max1) {
+                                                                    max2 = max1;
+                                                                                    max1 = n;
+                                                                                                } else if (n > max2) {
+                                                                                                                max2 = n;
+                                                                                                                            }
+                                                                                                                                    }
+                                                                                                                                            
+                                                                                                                                                    return (max1 - 1) * (max2 - 1);
+                                                                                                                                                        }
+                                                                                                                                                        }
+                                                                                                                                          
