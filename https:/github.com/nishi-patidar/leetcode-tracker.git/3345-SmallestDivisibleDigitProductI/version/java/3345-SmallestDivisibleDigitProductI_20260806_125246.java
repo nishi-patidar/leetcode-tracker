e75536aaf80376e8e8905214448cb9a156584048
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-06 12:52:47 +0530

    Create 3345-SmallestDivisibleDigitProductI

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3345-SmallestDivisibleDigitProductI/version/java/3345-SmallestDivisibleDigitProductI_20260806_125246.java
@@ -0,0 +1,20 @@
+// Last updated: 8/6/2026, 12:52:46 PM
+1class Solution {
+2    public int smallestNumber(int n, int t) {
+3        while (true) {
+4            int prod = 1;
+5            int temp = n;
+6            
+7            while (temp > 0) {
+8                prod *= temp % 10;
+9                temp /= 10;
+10            }
+11            
+12            if (prod % t == 0) {
+13                return n;
+14            }
+15            
+16            n++;
+17        }
+18    }
+19}
