200e535166e0245db640f3bb711cacffcb16dee2
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-11 13:50:41 +0530

    Create 1617-StoneGameIv

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1617-StoneGameIv/1617-StoneGameIv.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 1:50:45 PM
+class Solution {
+        public boolean winnerSquareGame(int n) {
+                boolean[] dp = new boolean[n + 1];
+                        
+                                for (int i = 1; i <= n; i++) {
+                                            for (int k = 1; k * k <= i; k++) {
+                                                            if (!dp[i - k * k]) {
+                                                                                dp[i] = true;
+                                                                                                    break;
+                                                                                                                    }
+                                                                                                                                }
+                                                                                                                                        }
+                                                                                                                                                
+                                                                                                                                                        return dp[n];
+                                                                                                                                                            }
+                                                                                                                                                            }
+                                                                                                                                                            
