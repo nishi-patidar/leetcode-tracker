0fe834bf393ab1bcc2a465b1dd4d6fdde1268628
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-15 22:19:00 +0530

    Create 3702-LongestSubsequenceWithNon-ZeroBitwiseXOR

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR/version/java/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR_20260815_221908.java
@@ -0,0 +1,24 @@
+// Last updated: 8/15/2026, 10:19:08 PM
+1class Solution {
+2    public int longestSubsequence(int[] nums) {
+3        int xorSum = 0;
+4        boolean hasNonZero = false;
+5        
+6        for (int num : nums) {
+7            xorSum ^= num;
+8            if (num != 0) {
+9                hasNonZero = true;
+10            }
+11        }
+12        
+13        if (!hasNonZero) {
+14            return 0;
+15        }
+16        
+17        if (xorSum != 0) {
+18            return nums.length;
+19        }
+20        
+21        return nums.length - 1;
+22    }
+23}
