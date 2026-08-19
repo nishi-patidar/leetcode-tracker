fb17b724a7c3bd5ae87606118c381c4559073424
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-19 20:59:14 +0530

    Create 3225-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3225-LengthOfLongestSubarrayWithAtMostKFrequency/3225-LengthOfLongestSubarrayWithAtMostKFrequency.java
@@ -0,0 +1,24 @@
+// Last updated: 8/19/2026, 8:59:13 PM
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+            
+            while (freq.get(nums[right]) > k) {
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+                left++;
+            }
+            
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        
+        return maxLen;
+    }
+}
