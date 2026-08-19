8e181383454439aa64c25d8790f41e165c2d7f32
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-19 20:57:20 +0530

    Create 1386-CinemaSeatAllocation

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1386-CinemaSeatAllocation/version/java/1386-CinemaSeatAllocation_20260819_205719.java
@@ -0,0 +1,31 @@
+// Last updated: 8/19/2026, 8:57:19 PM
+1import java.util.HashMap;
+2import java.util.Map;
+3
+4class Solution {
+5    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+6        Map<Integer, Integer> map = new HashMap<>();
+7        
+8        for (int[] seat : reservedSeats) {
+9            if (seat[1] >= 2 && seat[1] <= 9) {
+10                map.put(seat[0], map.getOrDefault(seat[0], 0) | (1 << seat[1]));
+11            }
+12        }
+13        
+14        int ans = 2 * (n - map.size());
+15        
+16        for (int val : map.values()) {
+17            boolean left = (val & 60) == 0;
+18            boolean right = (val & 960) == 0;
+19            boolean middle = (val & 240) == 0;
+20            
+21            if (left && right) {
+22                ans += 2;
+23            } else if (left || right || middle) {
+24                ans += 1;
+25            }
+26        }
+27        
+28        return ans;
+29    }
+30}
