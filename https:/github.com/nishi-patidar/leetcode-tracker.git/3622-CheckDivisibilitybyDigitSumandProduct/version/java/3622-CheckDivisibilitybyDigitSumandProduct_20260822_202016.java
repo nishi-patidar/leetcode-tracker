48940cc0e0e4d0dcf34cf90c5f92e65e60523547
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-22 20:20:16 +0530

    Create 3622-CheckDivisibilitybyDigitSumandProduct

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3622-CheckDivisibilitybyDigitSumandProduct/version/java/3622-CheckDivisibilitybyDigitSumandProduct_20260822_202016.java
@@ -0,0 +1,18 @@
+// Last updated: 8/22/2026, 8:20:16 PM
+1class Solution {
+2    public boolean checkDivisibility(int n) {
+3        int sum = 0;
+4        int product = 1;
+5        int temp = n;
+6        
+7        while (temp > 0) {
+8            int digit = temp % 10;
+9            sum += digit;
+10            product *= digit;
+11            temp /= 10;
+12        }
+13        
+14        return n % (sum + product) == 0;
+15    }
+16}
+17
