97240a61c2d2d60e80a8e39f92b9a969a2f991b6
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-24 20:51:46 +0530

    Create 77-Combinations

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/77-Combinations/77-Combinations.java
@@ -0,0 +1,24 @@
+// Last updated: 8/24/2026, 8:51:47 PM
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), 1, n, k);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int start, int n, int k) {
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        for (int i = start; i <= n - (k - current.size()) + 1; i++) {
+            current.add(i);
+            backtrack(result, current, i + 1, n, k);
+            current.remove(current.size() - 1);
+        }
+    }
+}
