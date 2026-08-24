223a2005e3c47d9b35b2625b5f9715e5e68626bc
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-24 20:53:05 +0530

    Create 1872-StoneGameVIII

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1872-StoneGameVIII/version/java/1872-StoneGameVIII_20260824_205306.java
@@ -0,0 +1,18 @@
+// Last updated: 8/24/2026, 8:53:06 PM
+1class Solution {
+2    public int stoneGameVIII(int[] stones) {
+3        int n = stones.length;
+4        
+5        for (int i = 1; i < n; i++) {
+6            stones[i] += stones[i - 1];
+7        }
+8        
+9        int res = stones[n - 1];
+10        
+11        for (int i = n - 2; i > 0; i--) {
+12            res = Math.max(res, stones[i] - res);
+13        }
+14        
+15        return res;
+16    }
+17}
