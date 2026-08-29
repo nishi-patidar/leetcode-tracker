f83779a8ddeb71ed607b179d154d6f239a0ad186
Nishi Patidar <27nishipatidar@gmail.com>
2026-08-29 23:38:12 +0530

    Create

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/problem-1788026893188/version/java/problem-1788026893188_20260829_233813.java
@@ -0,0 +1,16 @@
+// Last updated: 8/29/2026, 11:38:13 PM
+1class Solution {
+2    public int minBishopMoves(int[] source, int[] target) {
+3        int r1 = source[0], c1 = source[1];
+4        int r2 = target[0], c2 = target[1];
+5
+6        if ((r1 + c1) % 2 != (r2 + c2) % 2){
+7            return -1;
+8        }
+9        if (Math.abs(r1 - r2) == Math.abs(c1 - c2)){
+10            return 1;
+11        }
+12        return 2;
+13        }
+14    }
+15
