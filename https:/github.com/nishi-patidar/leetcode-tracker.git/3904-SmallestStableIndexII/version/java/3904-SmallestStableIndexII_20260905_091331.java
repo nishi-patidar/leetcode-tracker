a72aad42d111450a85a007049030a536c772e262
Nishi Patidar <27nishipatidar@gmail.com>
2026-09-05 09:13:31 +0530

    Create 3904-SmallestStableIndexII

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3904-SmallestStableIndexII/version/java/3904-SmallestStableIndexII_20260905_091331.java
@@ -0,0 +1,28 @@
+// Last updated: 9/5/2026, 9:13:31 AM
+1class Solution {
+2    public int firstStableIndex(int[] nums, int k) {
+3        int n = nums.length;
+4        int[] minRight = new int[n];
+5        int currentMin = nums[n - 1];
+6        minRight[n - 1] = currentMin;
+7        
+8        for (int i = n - 2; i >= 0; i--) {
+9            if (nums[i] < currentMin) {
+10                currentMin = nums[i];
+11            }
+12            minRight[i] = currentMin;
+13        }
+14        
+15        int maxLeft = nums[0];
+16        for (int i = 0; i < n; i++) {
+17            if (nums[i] > maxLeft) {
+18                maxLeft = nums[i];
+19            }
+20            if (maxLeft - minRight[i] <= k) {
+21                return i;
+22            }
+23        }
+24        
+25        return -1;
+26    }
+27}
