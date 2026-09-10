0ba08f33b776a73c54f2d3201974f3978982df38
Nishi Patidar <27nishipatidar@gmail.com>
2026-09-10 19:50:36 +0530

    Create 2265-CountNodesEqualtoAverageofSubtree

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/2265-CountNodesEqualtoAverageofSubtree/version/java/2265-CountNodesEqualtoAverageofSubtree_20260910_195035.java
@@ -0,0 +1,27 @@
+// Last updated: 9/10/2026, 7:50:35 PM
+1class Solution {
+2    int res = 0;
+3    
+4    public int averageOfSubtree(TreeNode root) {
+5        dfs(root);
+6        return res;
+7    }
+8    
+9    private long dfs(TreeNode node) {
+10        if (node == null) {
+11            return 0L;
+12        }
+13        
+14        long left = dfs(node.left);
+15        long right = dfs(node.right);
+16        
+17        int sum = (int) (left >>> 32) + (int) (right >>> 32) + node.val;
+18        int count = (int) left + (int) right + 1;
+19        
+20        if (sum / count == node.val) {
+21            res++;
+22        }
+23        
+24        return ((long) sum << 32) | count;
+25    }
+26}
