ebec99b738ec4761ebe1c5d6266f6f3042227f72
Nishi Patidar <27nishipatidar@gmail.com>
2026-09-16 10:58:23 +0530

    Create 1621-NumberofSetsofKNon-OverlappingLineSegments

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/1621-NumberofSetsofKNon-OverlappingLineSegments/version/java/1621-NumberofSetsofKNon-OverlappingLineSegments_20260916_105822.java
@@ -0,0 +1,35 @@
+// Last updated: 9/16/2026, 10:58:22 AM
+1class Solution {
+2    public int numberOfSets(int n, int k) {
+3        int mod = 1000000007;
+4        int N = n + k - 1;
+5        int K = 2 * k;
+6        
+7        if (K > N) {
+8            return 0;
+9        }
+10        
+11        long num = 1;
+12        long den = 1;
+13        
+14        for (int i = 1; i <= K; i++) {
+15            num = (num * (N - i + 1)) % mod;
+16            den = (den * i) % mod;
+17        }
+18        
+19        return (int) ((num * power(den, mod - 2, mod)) % mod);
+20    }
+21    
+22    private long power(long base, long exp, int mod) {
+23        long res = 1;
+24        base %= mod;
+25        while (exp > 0) {
+26            if ((exp & 1) == 1) {
+27                res = (res * base) % mod;
+28            }
+29            base = (base * base) % mod;
+30            exp >>= 1;
+31        }
+32        return res;
+33    }
+34}
