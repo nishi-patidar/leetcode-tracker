92613fbc8fc299c33ae6b33ef8b8980be2fd5a63
Nishi Patidar <27nishipatidar@gmail.com>
2026-09-24 12:41:12 +0530

    Create 3550-SmallestIndexWithDigitSumEqualtoIndex

---
+++ https:/github.com/nishi-patidar/leetcode-tracker.git/3550-SmallestIndexWithDigitSumEqualtoIndex/version/java/3550-SmallestIndexWithDigitSumEqualtoIndex_20260924_124113.java
@@ -0,0 +1,14 @@
+// Last updated: 9/24/2026, 12:41:13 PM
+1class Solution {
+2    public int smallestIndex(int[] nums) {
+3        for (int i = 0; i < nums.length; i++) {
+4            int n = nums[i];
+5            int sum = (n % 10) + ((n / 10) % 10) + ((n / 100) % 10) + (n / 1000);
+6            
+7            if (sum == i) {
+8                return i;
+9            }
+10        }
+11        return -1;
+12    }
+13}
